7cfb2f8b5c0688ae345f82d0d78686bbaf38ee2d
Admin <Admin@DESKTOP-8I89UR1>
2021-03-31 05:13:55 +0900

    first commet

---
+++ csvParseSample1/src/main/java/com/naver/dja12123/csvParseSample/EarthQuakeData.java
@@ -0,0 +1,36 @@
+package com.naver.dja12123.csvParseSample;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvDate;
+
+public class EarthQuakeData implements Comparable<EarthQuakeData>
+{
+
+	private static final long serialVersionUID = -1463918390087998017L;
+	@CsvBindByName(column = "#진원시")
+	@CsvDate("yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime dateTime;
+	@CsvBindByName(column = "규모")
+	private double magnitude;
+	@CsvBindByName(column = "위도", capture = "([^ ]+) .*")
+	private double latitude;
+	@CsvBindByName(column = "경도", capture = "([^ ]+) .*")
+	private double longitude;
+	@CsvBindByName(column = "위치")
+	private String description;
+	
+	@Override
+	public int compareTo(EarthQuakeData other)
+	{
+		return this.dateTime.compareTo(this.dateTime);
+	}
+}
